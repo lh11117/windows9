adcc2403e0dad99d7f6baf2392ca879e4d0092c8
lh11117 <linchh117@outlook.com>
2023-10-21 11:15:38 +0800

    优化
    - 开始菜单完善
    - 重启页面
    - 自动加载资源
    - 完善 README.md

--- kernel.js
+++ kernel.js
@@ -2,7 +2,7 @@
 
 function UpdateTime() {
     var date = new Date();
-    var now =  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "\n" + date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
+    var now = date.getHours() + ":" + (date.getMinutes().toString().length == 1 ? ('0' + date.getMinutes().toString()) : date.getMinutes().toString()) + ":" + (date.getSeconds().toString().length == 1 ? ('0' + date.getSeconds().toString()) : date.getSeconds().toString()) + "\n" + date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
     document.getElementById('taskbar-time-show').innerText = now;
 }
 
